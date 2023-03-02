591fd20bc5d1246e00bf73ec06aa8da9c87a7036
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2023-03-02 17:13:53 +0000

    Implemented the generation of a lexicon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import warnings
 import spacy
 from csv import reader
+from nltk.stem import WordNetLemmatizer
+
 
 def make_bow(words):
     bag = {}
@@ -45,19 +47,21 @@ def extract_training_text(csvfile):
 
     return training_text
 
-#Function 4
+
+# Function 4
 def get_ERT_emotions(csvfile):
+    # TODO- Maybe extract the scores too?
     data = []
-    with open(csvfile,"r") as f:
+    with open(csvfile, "r") as f:
         file_reader = reader(f)
         for i in file_reader:
             data.append(i)
 
-    considered=data[4:]
+    considered = data[4:]
     emotions = []
 
     for record in considered:
-        record_emotions=[]
+        record_emotions = []
         for value in record[27:77:5]:
             record_emotions.append(value)
         emotions.append(record_emotions)
@@ -70,12 +74,25 @@ def get_ERT_emotions(csvfile):
     return emotions
 
 
+def generate_lexicon(emotions):
+    lexicon = []
+    wnl = WordNetLemmatizer()
+
+    for emotion_record in emotions:
+        for emotion in emotion_record:
+            comparison_emotion = wnl.lemmatize(emotion.lower())
+            if comparison_emotion not in lexicon:
+                lexicon.append(comparison_emotion)
+
+    return lexicon
+
+
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     nlp = spacy.load("en_core_web_sm")
     test = extract_training_text("sectraining.csv")
     doc = tokenize_text(nlp, test[0][0])
-    new_doc=filter_tags(doc)
-    ERT= get_ERT_emotions("ERT_dataset.csv")
-    for emotion in ERT:
-        print(emotion)
+    new_doc = filter_tags(doc)
+    ERT = get_ERT_emotions("ERT_dataset.csv")
+    lex = generate_lexicon(ERT)
+
