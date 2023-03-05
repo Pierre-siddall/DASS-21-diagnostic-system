b67dfde5d164e0187d1ce50c0435742fb2226246
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2023-03-05 18:41:08 +0000

    Implementation of the generation of a vector for a document alongside creation of a text file which will hold the amount of documents a certain word appears in across the whole corpus

--- main.py
+++ main.py
@@ -1,12 +1,82 @@
 import warnings
 import spacy
+import json
+import numpy
+import math
 from csv import reader
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords, wordnet
-from sklearn.neural_network import MLPRegressor
+
+
+# TODO - Make unit tests, for calculate doc vector and find word antonym
+
+
+# Function 11
+def make_bow(discoveries):
+    bow = {}
+
+    for term in discoveries:
+        if term not in bow.keys():
+            bow[term] = 1
+        elif term in bow.keys():
+            bow[term] += 1
+
+    return bow
+
+
+# Function 10
+def calculate_doc_vector(corpus, doc, lexicon, nlp_model, stopwords):
+    vector = []
+
+    with open("corpus_frequency.txt", "r") as f:
+        text = f.read()
+        frequencies = json.load(text)
+        f.close()
+
+    doc_discoveries = make_bow(discover_emotional_words(doc, lexicon, nlp_model, stopwords))
+
+    for key in doc_discoveries.keys():
+        tf_doc_term_freq = doc_discoveries[key]
+        tf_doc_sum = sum(doc_discoveries.values())
+
+        tf_value = tf_doc_term_freq / tf_doc_sum
+
+        idf_num_docs = len(corpus)
+        idf_appearances = frequencies[key]
+
+        idf_value = math.log(idf_num_docs / idf_appearances)
+
+        tfidf_value = tf_value * idf_value
+
+        vector.append(tfidf_value)
+
+    return vector
 
 
 # Function 9
+def get_corpus_frequency(corpus, lexicon, nlp_model, stopwords):
+    all = {}
+    count = 1
+
+    for text in corpus:
+        print(f"Reached text {count} out of {len(corpus)}")
+        count += 1
+        token_text = tokenize_text(nlp_model, text[0])
+        discover_words = discover_emotional_words(token_text, lexicon, nlp_model, stopwords)
+        word_set = set(discover_words)
+
+        for word in word_set:
+            if word not in all.keys():
+                all[word] = 1
+            elif word in all.keys():
+                all[word] += 1
+
+    with open("corpus_frequency.txt", "w") as f:
+        f.write(json.dumps(all))
+        f.close()
+
+
+# Function 8
 def get_max_similarity(sims):
     max_score = -1
     max_text = ''
@@ -19,7 +89,7 @@ def get_max_similarity(sims):
     return max_score, max_text
 
 
-# Function 8
+# Function 7
 def find_word_antonym(token):
     antonyms = []
     for syn in wordnet.synsets(token):
@@ -27,10 +97,13 @@ def find_word_antonym(token):
             if lem.antonyms():
                 antonyms.append(lem.antonyms()[0].name())
 
-    return antonyms[0]
+    if len(antonyms) == 0:
+        return
+    elif len(antonyms) > 0:
+        return antonyms[0]
 
 
-# Function 7
+# Function 6
 def is_negated(token):
     if token.dep_ == 'neg':
         return True
@@ -38,24 +111,13 @@ def is_negated(token):
         return False
 
 
-# Function 6
-def make_bow(words):
-    bag = {}
-
-    for word, tag in words:
-        if word not in bag.keys():
-            bag[word] = 1
-        else:
-            bag[word] += 1
-
-    return bag
-
-
 # Function 5
 def discover_emotional_words(doc, lexicon, nlp_model, stopwords):
     discovered_words = []
 
-    for token in doc:
+    filtered_doc = filter_tags(doc)
+
+    for token in filtered_doc:
         base = nlp_model(token.text)
         similarities = []
         for emotion in lexicon:
@@ -63,21 +125,24 @@ def discover_emotional_words(doc, lexicon, nlp_model, stopwords):
             comparison = nlp_model(emotion)
             negated = is_negated(token)
 
-            if token.text not in stopwords:
-                if negated:
-                    new_comparison = nlp_model(find_word_antonym(comparison.text))
-                    sim_score = base.similarity(new_comparison)
-                    similarities.append((new_comparison.text, sim_score))
-                else:
-                    sim_score = base.similarity(comparison)
-                    similarities.append((comparison.text, sim_score))
+            try:
+                if token.text not in stopwords and type(base) is not None and type(comparison) is not None:
+                    if negated:
+                        new_comparison = nlp_model(find_word_antonym(comparison.text))
+                        sim_score = base.similarity(new_comparison)
+                        similarities.append((new_comparison.text, sim_score))
+                    else:
+                        sim_score = base.similarity(comparison)
+                        similarities.append((comparison.text, sim_score))
+            except:
+                pass
 
         score_max, text_max = get_max_similarity(similarities)
 
         if score_max >= 0.5:
             discovered_words.append(text_max)
 
-        print(discovered_words)
+    return discovered_words
 
 
 # Function 3
@@ -157,10 +222,9 @@ if __name__ == '__main__':
     nlp = spacy.load("en_core_web_sm")
     swords = stopwords.words('english')
 
-    test = extract_training_text("sectraining.csv")
-    doc = tokenize_text(nlp, test[0][0])
-    new_doc = filter_tags(doc)
+    corpus = extract_training_text("sectraining.csv")
+    doc = tokenize_text(nlp, corpus[0][0])
     ERT = get_ERT_emotions("ERT_dataset.csv")
     lex = generate_lexicon(ERT)
 
-    discover_emotional_words(new_doc, lex, nlp, swords)
+    discover_emotional_words(doc, lex, nlp, swords)
